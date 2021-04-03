4291ab4342c95bce803a97dfe37ba8b7c2e20b94
Beatriz Ferreira dos Santos <bfsantos@RDM503728.local>
2021-04-03 18:37:14 -0300

    corrigindo put

--- src/index.js
+++ src/index.js
@@ -31,7 +31,7 @@ app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
   const { title, url, techs} = request.body;
 
-  const repositoryIndex = repositories.findindex(repository => repository.id === id);
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
   if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
